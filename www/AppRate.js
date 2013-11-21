9d0803dc17601ad153cac96d288d11e193745118
zraza <zeeshan_raza@yahoo.com>
2013-11-21 04:29:04 +0000

    xx

--- www/AppRate.js
+++ www/AppRate.js
@@ -3,20 +3,20 @@ var AppRate = (function() {
     var _api = {};
     var _initCount = 1;
     var _config = {
-        waitCount = 3,
-        iosAppId = '',
-        androidAppId = '',
+        waitCount: 3,
+        iosAppId: '',
+        androidAppId: '',
         title: "Rate application",
         message: "If you enjoy using this application, would you mind taking a moment to rate it? It won't take more than a minute. Thanks for your support!",
         actions: ["Rate It Now", "Remind Me Later", "No, Thanks"]
 
-    }
+    };
     var _init = function(config) {
 
-    }
+    };
     var _disablePrompt = function() {
 
-    }
+    };
     var _navigateToAppStore = function() {
         if (/(iPhone|iPod|iPad)/i.test(navigator.userAgent.toLowerCase())) {
             return window.open("itms-apps://ax.itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?type=Purple+Software&id=" + _config.iosAppId);
@@ -35,7 +35,7 @@ var AppRate = (function() {
             case 3:
                 _disablePrompt();
         }
-    }
+    };
 
     var _prompt = function() {
         if (_initCount++ % (_config.waitCount + 1) === 0) {
@@ -45,7 +45,6 @@ var AppRate = (function() {
     _api.init = _init;
     _api.navigateToAppStore = _navigateToAppStore;
     _api.prompt = _prompt;
+    return _api;
 
 })();
-
-module.exports = AppRate; //new AppRate(this);
